ea49bbe9ded58888410f2be8640375a92379e570
Tianyi Chen <TianyiChen@users.noreply.github.com>
2020-04-17 13:24:38 -0700

    Update clang.sh

--- llvm/clang.sh
+++ llvm/clang.sh
@@ -5,5 +5,5 @@ mkdir build
 cd build
 export CC=clang-11
 rm CMakeCache.txt;export CXX="clang-11 -w -stdlib=libc++ -lc++ -lc++abi  -lstdc++ -L/usr/lib/llvm-11/lib"
-cmake -DLLVM_ENABLE_PROJECTS=clang -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -G "Unix Makefiles" ../llvm
+cmake -DLLVM_ENABLE_PROJECTS="clang;compiler-rt" -DCMAKE_BUILD_TYPE=Release -DLLVM_TARGETS_TO_BUILD=X86 -G "Unix Makefiles" ../llvm
 make -j24
